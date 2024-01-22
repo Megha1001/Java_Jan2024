ca6ce1983c3f8493ea952eef4e8406422f2e464f
Megha Verma <meghaverma1001@gmail.com>
2024-01-22 18:06:57 +0530

    Count of digits

---
+++ .idea/mathematics/CountOfDigits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountOfDigits {
+    public static void main(String[] args) {
+        Scanner inputObj = new Scanner(System.in);
+        System.out.println("Enter the number please :");
+        String inputString = inputObj.nextLine();
+        System.out.println("Count of digits the number is : "+countOfDigits(getInputNumber(inputString)));
+    }
+
+    private static int getInputNumber(String inputString) {
+        return inputString.length()>0 ? Integer.parseInt(inputString) : 0;
+    }
+
+    // Time complexity : Theta(length_of_input)
+    public static int countOfDigits(int number){
+        int result = 0;
+        while(number>0){
+            result ++;
+            number /= 10;
+        }
+        return result;
+    }
+}
