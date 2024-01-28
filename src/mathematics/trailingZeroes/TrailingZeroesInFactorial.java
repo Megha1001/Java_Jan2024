de3071335a32f6616b9cd956bcfdc14ab82670e4
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2024-01-28 17:10:23 +0530

    Create TrailingZeroesInFactorial.java

---
+++ src/mathematics/trailingZeroes/TrailingZeroesInFactorial.java
@@ -0,0 +1,37 @@
+package mathematics.trailingZeroes;
+
+import java.util.Scanner;
+/*
+  n>0
+ */
+public class TrailingZeroesInFactorial {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number :");
+    int num = Integer.parseInt(sc.nextLine());
+    System.out.println("Trailing zeroes in factorial of "+num+" is "+countTrailingZeroes(num));
+
+  }
+
+  // Have problem of overflow
+  public static int countTrailingZeroes(int num){
+    int factorial = findFactorial(num);
+    System.out.println(factorial);
+    int result = 0;
+    while(factorial%10==0 && factorial > 0){
+      ++result;
+      factorial /= 10;
+    }
+    return result;
+  }
+
+  public static int findFactorial(int num){
+    int result = 1;
+    for(int i = 2; i <= num; i++){
+      result = result*i;
+    }
+    return result;
+  }
+
+}
