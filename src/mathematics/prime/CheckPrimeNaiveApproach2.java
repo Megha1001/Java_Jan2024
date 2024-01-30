80df1653bb91ab3f6c76c0c61406de515c62118a
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2024-01-30 21:14:34 +0530

    Create CheckPrimeNaiveApproach2.java

---
+++ src/mathematics/prime/CheckPrimeNaiveApproach2.java
@@ -0,0 +1,20 @@
+package mathematics.prime;
+
+public class CheckPrimeNaiveApproach2 {
+  public static void main(String[] args) {
+    int input = 101;
+    System.out.println("The enterned number "+input+" is "+checkPrime(input));
+  }
+
+  public static String checkPrime(int n){
+    if(n == 1 || n%2 == 0 || n%3 == 0){
+      return " not a Prime.";
+    }
+    for(int i=2; i <=Math.sqrt(n); i++){
+      if(n%i ==0){
+        return " not a Prime.";
+      }
+    }
+    return " Prime.";
+  }
+}
