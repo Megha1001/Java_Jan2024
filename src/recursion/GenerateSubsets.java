516ea04abb0a721b3d5f0b222eba9d25c02ee5f3
Megha Verma <meghaverma1001@gmail.com>
2024-03-05 19:45:29 +0530

    Generate Subsets

---
+++ src/recursion/GenerateSubsets.java
@@ -0,0 +1,23 @@
+package recursion;
+
+public class GenerateSubsets {
+    public static void main(String[] args) {
+        String input = "ABC";
+        String curr = "";
+        int index = 0;
+        System.out.println("All subsets/subsequence of given input are :");
+        printSubsets(input,"", index);
+    }
+
+    public static void printSubsets(String input, String curr, int index){
+        //base case
+        if(index == input.length()){
+            System.out.print(curr+", ");
+            return;
+        }
+
+        printSubsets(input, curr, index+1);//not considering
+        printSubsets(input, curr+input.charAt(index), index+1);//considering
+    }
+
+}
