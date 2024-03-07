361a6d8da0465843725113636a3ae472e5cf0062
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2024-03-07 20:15:48 +0530

    Update SecondLargest.java

--- src/arrays/SecondLargest.java
+++ src/arrays/SecondLargest.java
@@ -4,8 +4,8 @@ TIME COMPLEXITY : O(N)
 class SecondLargest {
 
     public static void main(String[] args) {
-        int arr[]= {1,2,5,7,3,0,9};
-        
+        // int arr[]= {1,2,5,7,3,0,9};
+        int arr[]= {10,10,10,10};
         System.out.println("Get second Largest -> "+getSecondLargest(arr));
        
     }
@@ -17,7 +17,7 @@ class SecondLargest {
                 max = arr[i];
             }
         }
-        int secondLargest = max!=arr[0] ? arr[0] : arr[1];
+        int secondLargest = -1; //When all are same then -1
         for(int i=1; i < arr.length; i++){
             if((arr[i] > secondLargest) && (arr[i] != max)){
                 secondLargest = arr[i];
