679229eed630c0377816c551a69bb8175fbb2dbc
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2024-03-07 19:11:30 +0530

    Create GetLargest.java

---
+++ src/arrays/GetLargest.java
@@ -0,0 +1,24 @@
+/*
+TIME COMPLEXITY : O(N)
+*/
+class GetLargest {
+
+    public static void main(String[] args) {
+        int arr[]= {1,2,5,7,3,0,9};
+        
+        System.out.println("Get largetst -> "+getLargest(arr));
+       
+    }
+    
+    public static int getLargest(int arr[]){
+        int res = arr[0];
+        for(int i=1; i < arr.length; i++){
+            if(arr[i] > res){
+                res = arr[i];
+            }
+        }
+        
+        return res;
+    }
+
+}
