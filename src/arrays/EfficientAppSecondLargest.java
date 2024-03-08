0b208462771626614e9f51d90f265ef852583f02
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2024-03-08 15:28:00 +0530

    Create EfficientAppSecondLargest.java

---
+++ src/arrays/EfficientAppSecondLargest.java
@@ -0,0 +1,21 @@
+class EfficientAppSecondLargest{
+    public static void main(String [] args){
+        int arr[] = {3,5,6,0,-1,4,8,10,19};
+        int indexSecondLargest = findSecondLargest(arr);
+        System.out.println("The second largest in given array is "+(indexSecondLargest!=-1?arr[indexSecondLargest]: "Not present"));
+    }
+    
+    public static int findSecondLargest(int arr[]){
+        int res = -1;
+        int largest = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > arr[largest]){
+                res = largest;
+                largest = i;
+            }else if(arr[i] < arr[largest] && (res == -1 || arr[res] < arr[i])){
+               res = i;
+            }
+        }
+        return res;
+    }
+}
