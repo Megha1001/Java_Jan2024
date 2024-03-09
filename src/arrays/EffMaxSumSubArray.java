b78436e83311a34388b2a260144c8f459eb01b71
Megha Verma <meghaverma1001@gmail.com>
2024-03-09 21:52:05 +0530

    Create EffMaxSumSubArray.java

---
+++ src/arrays/EffMaxSumSubArray.java
@@ -0,0 +1,27 @@
+package arrays;
+/*
+TIME COMPLEXITY : O(N)
+The idea is to maintain a variable maxEnding that stores the maximum sum contiguous subarray
+ending at the current index and a variable res stores the maximum sum of contiguous subarray
+found so far.
+Everytime there is a positive-sum value in maxEnding comapre it with res and update res if its
+greater than res.
+
+ */
+public class EffMaxSumSubArray {
+    public static void main(String[] args) {
+        int arr[] ={2,3,-8,7,-1,2,3};
+        System.out.println("Maximum sum of subarray is :"+findAndGetMaxSum(arr));
+    }
+
+    public static int findAndGetMaxSum(int arr[]){
+        int res = arr[0];
+        int maxEnding = arr[0];
+        for(int i=1; i<arr.length; i++){
+            maxEnding = Math.max(maxEnding+arr[i],arr[i]);
+            res = Math.max(res, maxEnding);
+        }
+
+        return res;
+    }
+}
