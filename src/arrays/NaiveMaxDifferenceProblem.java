e122e54590698074a5cb9126ae125b9ed68163aa
Megha Verma <meghaverma1001@gmail.com>
2024-03-09 12:48:46 +0530

    Create NaiveMaxDifferenceProblem.java

---
+++ src/arrays/NaiveMaxDifferenceProblem.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class NaiveMaxDifferenceProblem {
+    public static void main(String[] args) {
+        int arr[] = {2,3,10,6,4,8,1};
+        System.out.println("Maximum difference b/w two pairs is "+getMaxDiff(arr));
+    }
+    
+    public static int getMaxDiff(int arr[]){
+        int res = arr[1]-arr[0];
+        for(int i=0; i<arr.length - 1; i++){ //NO need to do this for last element.
+            for(int j=i+1; j<arr.length; j++){
+                int diffTemp = arr[j]-arr[i];
+                if(diffTemp >res){
+                    res = diffTemp;
+                }
+            }
+        }
+        return res;
+    }
+}
