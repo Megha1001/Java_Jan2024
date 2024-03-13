9294a14ef8e20f0732a5aaeb0e39ed39b44b1291
Megha Verma <meghaverma1001@gmail.com>
2024-03-14 00:04:50 +0530

    Create BinarySearchSolution.java

---
+++ src/searching/sortedRotatedArray/BinarySearchSolution.java
@@ -0,0 +1,49 @@
+package searching.sortedRotatedArray;
+
+public class BinarySearchSolution {
+    public static void main(String[] args) {
+        /*int arr[] = {10,20,30,40,50,8,9};
+        int x = 30;*/
+/*        int arr[] = {100, 200, 300, 10, 20};
+        int x = 40;*/
+      /*  int arr[] = {100, 200, 300, 10, 20};
+        int x = 100;*/;
+
+        int arr[] = {100, 200, 300, 400, 20, 30, 40};
+        int x = 50;
+        System.out.println("the index of the search element is : "+search(arr, 0, arr.length - 1, x));
+    }
+
+    public static int search(int arr[], int l, int h, int x){
+
+        while(l <= h){
+            int m = (l+h)/2;
+
+            //check for middle
+            if(arr[m] == x){
+                return m;
+            }
+
+            //check which part is sorted - is it left ?
+            if(arr[m] >= arr[l]){ //left half is sorted
+                // is x lies b/w them ?
+                if(x < arr[m] && x>=arr[l]){
+                    //go to left
+                    h = m - 1;
+                }else{
+                    // go to right
+                    l = m + 1;
+                }
+            }else if (arr[m] <= arr[h]){//check which part is sorted - is it right ?
+                // is x lies b/w them ?
+                if(x > arr[m] && x <= arr[h]){
+                    l = m + 1;
+                }else{
+                   //go to left
+                    h = m - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
