a47cd64be57de892f87addc3164942c0881ee105
Megha Verma <meghaverma1001@gmail.com>
2024-03-14 19:48:36 +0530

    Create EfficientAppUsingBinarySearch.java

---
+++ src/searching/searchTriplet/EfficientAppUsingBinarySearch.java
@@ -0,0 +1,42 @@
+package searching.searchTriplet;
+
+/*
+ * TIME COMPLEXITY : O(N^2)
+ * Approach : 
+ * 1. Traverse the array from L to R for every element
+ * 2. for every element arr[i] search a pair on right side whose sum is x-arr[i], where x is the target sum
+ */
+public class EfficientAppUsingBinarySearch {
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 4, 8, 9, 20, 40};
+        int x = 32;
+        System.out.println("is the triplet exists with given item ? "+searchTriplet(arr, x));
+    }
+
+    public static boolean searchTriplet(int arr[], int x){
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            if(searchPair(arr, i+1, n-1, x-arr[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Two pointer approach
+    public static boolean searchPair(int arr[], int s, int e, int x){
+        while(s < e){
+            int sum = arr[s]+arr[e];
+            if(sum == x){
+                return true;
+            }else if(sum > x){
+                // move e
+                --e;
+            }else{
+                //move s
+                ++s;
+            }
+        }
+        return false;
+    }
+}
