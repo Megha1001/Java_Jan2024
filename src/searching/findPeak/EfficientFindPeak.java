00947a78ec1a11293e8e5d99a9a604cf9f1ca284
Megha Verma <meghaverma1001@gmail.com>
2024-03-14 16:31:51 +0530

    Create EfficientFindPeak.java

---
+++ src/searching/findPeak/EfficientFindPeak.java
@@ -0,0 +1,33 @@
+package searching.findPeak;
+
+/*
+ * A peak element is an element whose neighbours have value smaller than that of the element
+ */
+
+public class EfficientFindPeak{
+    public static void main(String [] args){
+        int arr[] = {5,20,40,30,20,50,60};
+        System.out.println("The peak element in given array is :"+findPeak(arr, 0, arr.length-1));
+    }
+
+    public static int findPeak(int arr[], int l, int h){
+
+        while(l <= h){
+            int m = (l+h)/2;
+
+            //check for middle
+            if((m==0 || arr[m]>=arr[m-1]) && (m==arr.length-1 || arr[m]>arr[m+1])){
+                return m;
+            }
+
+            //go to left
+            if(m > 0 && arr[m] <= arr[m-1]){
+                h = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+
+        return -1;
+    }
+}
