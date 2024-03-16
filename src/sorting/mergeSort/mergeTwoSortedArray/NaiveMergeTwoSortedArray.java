7e8a5dc732b5ae6684a4c0ad992520a555503576
Megha Verma <meghaverma1001@gmail.com>
2024-03-16 18:12:08 +0530

    Create NaiveMergeTwoSortedArray.java

---
+++ src/sorting/mergeSort/mergeTwoSortedArray/NaiveMergeTwoSortedArray.java
@@ -0,0 +1,38 @@
+package sorting.mergeSort.mergeTwoSortedArray;
+
+import java.util.Arrays;
+
+public class NaiveMergeTwoSortedArray {
+    public static void main(String args[]){
+        int a[] = {15,10,20,20};
+        int b[] = {1,12};
+
+        System.out.println("Elements after merging two sorted array and after sorting it are :");
+        int c[] = mergeTwoSortedArray(a, b);
+        for(int i=0; i<c.length; i++){
+            System.out.print(c[i]+" ");
+        }
+    }
+
+    public static int[] mergeTwoSortedArray(int a[], int b[]){
+        int m = a.length;
+        int n = b.length;
+        int c[] = new int[m+n];
+
+        //copy elements of array a into c
+        for(int i=0; i<m; i++){
+            c[i] = a[i];
+        }
+
+        //copy elements of array b into c
+        for(int i=0; i<n; i++){
+            c[i+m] = b[i];
+        }
+
+        //Now sort
+        Arrays.sort(c); // takes O((m+n)LogN)
+
+        return c;
+    }
+    
+}
