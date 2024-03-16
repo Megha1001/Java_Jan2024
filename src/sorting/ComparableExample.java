8ec31a8587d966452a0cb77e255d7140cb85b55d
Megha Verma <meghaverma1001@gmail.com>
2024-03-16 08:49:46 +0530

    Create ComparableExample.java

---
+++ src/sorting/ComparableExample.java
@@ -0,0 +1,30 @@
+package sorting;
+
+
+import java.util.Arrays;
+
+public class ComparableExample {
+
+    public static void main(String[] args) {
+        Points [] points = {new Points(3,5), new Points(1,2), new Points(0,4), new Points(4,5)};
+        Arrays.sort(points);
+        for (int i=0; i< points.length; i++){
+            System.out.println(points[i].x+" ");
+        }
+    }
+
+}
+
+class Points implements Comparable<Points>{
+    int x;
+    int y;
+
+    Points(int x, int y){
+        this.x = x;
+        this.y =y;
+    }
+
+    public int compareTo(Points p){
+        return this.x - p.x;
+    }
+}
