91ca9c32b477fe3337fd8cfd1e142c76fe3c0fc6
Megha Verma <meghaverma1001@gmail.com>
2024-03-17 13:49:30 +0530

    Create NaiveCountInversion.java

---
+++ src/sorting/mergeSort/countInversion/NaiveCountInversion.java
@@ -0,0 +1,34 @@
+package sorting.mergeSort.countInversion;
+
+/*
+ * TIME COMPLEXITY : O(N^N)
+ */
+public class NaiveCountInversion {
+
+    public static void main (String[] args) 
+    {
+        int arr[] = new int[]{2,4,1,3,5};
+        
+        int n = arr.length;
+	    System.out.print(countInv(arr));
+        
+    }
+    
+
+    public static int countInv(int a[]){
+        int count = 0;
+        int n = a.length;
+
+        for(int i=0; i<n-1;i++){//run until n-2 since we have to consider j for n-1(last element)
+            for(int j=i+1; j<n; j++){
+                if(a[i] > a[j]){
+                    ++count;
+                }
+            }
+
+        }
+
+        return count;
+
+    }
+}
