3d872ec6a0a95d1dc63647375227aa89ffa516a0
Megha Verma <meghaverma1001@gmail.com>
2024-03-19 23:31:27 +0530

    Update SortAnArrayWithTwoTypesOfElements.java

--- src/sorting/SortAnArrayWithTwoTypesOfElements.java
+++ src/sorting/SortAnArrayWithTwoTypesOfElements.java
@@ -13,4 +13,32 @@ public class SortAnArrayWithTwoTypesOfElements {
             System.out.print(x+" ");
         
     }
+
+    //Hoare's partition
+
+    public static void sort(int arr[], int n){
+        int p = 0; //since we have to put -ve elements then postive element , that means sort around zero
+        int i = -1;
+        int j = n;
+
+        while(true){
+            do{
+                ++i;
+            }while(arr[i] < 0);
+
+            do{
+                --j;
+            }while(arr[j] >=0);
+
+            if(i>=j){
+                return;
+            }
+
+            //swap
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp; 
+        }
+
+    }
 }
