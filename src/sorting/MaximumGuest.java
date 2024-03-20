14879c19e489707fb47cb32be96b3925119bff5e
Megha Verma <meghaverma1001@gmail.com>
2024-03-21 00:04:57 +0530

    Update MaximumGuest.java

--- src/sorting/MaximumGuest.java
+++ src/sorting/MaximumGuest.java
@@ -1,4 +1,5 @@
 package sorting;
+import java.util.Arrays;
 
 public class MaximumGuest {
 
@@ -6,6 +7,39 @@ public class MaximumGuest {
 
         int arrival[] = {900, 600, 700};
         int departure[] = {1000, 800,730};
+
+        System.out.println("Max guests that we can meet is/are "+findMaxGuest(arrival, departure));
     }
+
+    public static int findMaxGuest(int arr[], int dep[]){
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int n = arr.length;
+        int i = 1; //arrival count;
+        int j = 0 ; //departure count;
+
+        int current = 1; //current guest;
+
+        int res = 1; //max guest till now
+
+        while(i < n && j < n){
+            //is the next is arrival
+            if(arr[i] <= dep[j]){ //the guest came just now has arrival time <= dep of previous
+                //increase number of guest
+                ++current;
+                ++i; //arrival
+            }else{
+                //departure
+                --current;
+                ++j;
+            }
+            res = Math.max(res, current);
+        }
+
+        return res;
+
+    }
+
     
 }
