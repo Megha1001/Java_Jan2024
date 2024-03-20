1770a56b2e90c2b073922a572452b122b7ce0324
Megha Verma <meghaverma1001@gmail.com>
2024-03-21 00:00:38 +0530

    Update MaximumGuest.java

--- src/sorting/MaximumGuest.java
+++ src/sorting/MaximumGuest.java
@@ -5,7 +5,7 @@ public class MaximumGuest {
     public static void main(String args[]){
 
         int arrival[] = {900, 600, 700};
-        int departure[] = {1000, 800,730}
+        int departure[] = {1000, 800,730};
     }
     
 }
