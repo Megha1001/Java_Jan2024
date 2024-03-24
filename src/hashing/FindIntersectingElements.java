e4d9baa0687ae38a2c98a0efbc34eea0f0acc408
Megha Verma <meghaverma1001@gmail.com>
2024-03-24 22:48:01 +0530

    Create FindIntersectingElements.java

---
+++ src/hashing/FindIntersectingElements.java
@@ -0,0 +1,31 @@
+package hashing;
+/*
+ * TIME COMPLEXITY : Theta(M+N)
+ */
+import java.util.HashSet;
+
+public class FindIntersectingElements {
+
+    public static void main(String [] args){
+        int a[] = {10,15,20,25,30,50};
+        int b[] = {30,5,15,80};
+        System.out.println("The intersecting elements are :-");
+        findIntersectingElements(a,b);
+    }
+    
+    public static void findIntersectingElements(int a[], int b[]){
+        int n = a.length;
+        HashSet<Integer> h1 = new HashSet<>();
+
+        for(int i=0; i<b.length; i++){
+            h1.add(b[i]);
+        }
+
+        for(int i=0; i<n; i++){
+            if(h1.contains(a[i])){  
+                System.out.print(a[i]+" ");
+            }
+        }
+
+    }
+}
