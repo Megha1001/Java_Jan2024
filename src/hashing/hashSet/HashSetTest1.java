7b8cb7e10da449a60dfc9817d3e34cd461fb0e80
Megha Verma <meghaverma1001@gmail.com>
2024-03-24 18:19:21 +0530

    Create HashSetTest1.java

---
+++ src/hashing/hashSet/HashSetTest1.java
@@ -0,0 +1,24 @@
+package hashing.hashSet;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class HashSetTest1 {
+    public static void main(String[] args) {
+        HashSet<String> h = new HashSet<>();
+        h.add("megha");
+        h.add("verma");
+        h.add("SE-II");
+
+        System.out.println(h.size());
+        System.out.println(h.contains("SE-II"));
+
+        for(String s : h){
+            System.out.println(s+" ");
+        }
+
+        System.out.println(h.isEmpty());
+        h.clear();
+        System.out.println(h.isEmpty());
+    }
+}
