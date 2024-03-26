bbe77ebc3af765c396a480f1d8c4963ab17c25b5
Megha Verma <meghaverma1001@gmail.com>
2024-03-27 00:07:07 +0530

    Update EffMajorityElement_MooreVotingAlgo.java

--- src/arrays/EffMajorityElement_MooreVotingAlgo.java
+++ src/arrays/EffMajorityElement_MooreVotingAlgo.java
@@ -7,7 +7,7 @@ This is a two-step process
     majority element in an array then this step will definitely return majority element, otherwise it
     will return the candidate for majority element
 
-    2. Check fi the element obtained from the above step is the majority element. This step is necessary
+    2. Check if the element obtained from the above step is the majority element. This step is necessary
     as there might be no majority element
 
     Disclaimer : This algo doesn't gurantee to return the index of first occurrence of majority element
@@ -16,8 +16,10 @@ This is a two-step process
  */
 public class EffMajorityElement_MooreVotingAlgo {
     public static void main(String[] args) {
-        int arr[]={8,8,6,6,6,6,4,6};
-        System.out.println("The majority element is : "+arr[countMajorityElementUsingMooreAlgo(arr)]);
+        // int arr[]={8,8,6,6,6,6,4,6};
+        int arr[]={8,8,8,6,6,6};
+        int res = countMajorityElementUsingMooreAlgo(arr);
+        System.out.println("The majority element is : "+(res!=-1 ?arr[res] : "Not exists"));
     }
 
     public static int countMajorityElementUsingMooreAlgo(int arr[]){
@@ -45,6 +47,8 @@ public class EffMajorityElement_MooreVotingAlgo {
         return res;
     }
 
+    //specifically to use when there is no majority
+
     public static int checkAndGetIfMajorityOrNot(int arr[], int candidateIndex){
         int count=0;
         int res = -1;
@@ -53,6 +57,10 @@ public class EffMajorityElement_MooreVotingAlgo {
                 ++count;
             }
         }
+
+        /*
+         * IF element appears <= n/2 times that means its a part of set that cancelled out each other not the part of set that has majority element
+         */
 
         return count <= arr.length/2 ? res : candidateIndex;
     }
