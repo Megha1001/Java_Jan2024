c8a26965ba0bd6386ba44157077c959ef42c4c89
Megha Verma <meghaverma1001@gmail.com>
2024-03-27 22:14:10 +0530

    Update App1PalindromeCheck.java

--- src/strings/palindrome/App1PalindromeCheck.java
+++ src/strings/palindrome/App1PalindromeCheck.java
@@ -1,17 +1,17 @@
-package strings;
+package strings.palindrome;
 
 public class App1PalindromeCheck {
     
     public static void main(String args[]){
-        String s = "AAABBBCCC";
+        String s = "ABCDCBA";
 
-        System.out.println("is given string palindrome ?"+isPalindrome(s));
+        System.out.println("is given string palindrome ? "+isPalindrome(s));
     }
 
     public static boolean isPalindrome(String s){
 
         for(int i=0; i<(s.length())/2; i++){
-            if(!(s.charAt(i)==s.charAt(s.length()-1-i))){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
                 return false;
             }
         }
