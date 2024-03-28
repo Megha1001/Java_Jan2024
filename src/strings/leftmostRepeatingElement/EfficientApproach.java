5f8792305ead65e301fff0bb5d6708d2c4d2912e
Megha Verma <meghaverma1001@gmail.com>
2024-03-28 22:34:23 +0530

    Create EfficientApproach.java

---
+++ src/strings/leftmostRepeatingElement/EfficientApproach.java
@@ -0,0 +1,27 @@
+package strings.leftmostRepeatingElement;
+
+public class EfficientApproach {
+
+    public static final int CHAR = 256;
+    public static void main(String args[]){
+        String s = "meghaverma"; //m
+
+        System.out.println("The index of leftmost repeating element is :"+findLeftMostRepeatingElement(s));
+    }
+
+    public static int findLeftMostRepeatingElement(String s){
+        int count[] = new int[CHAR];
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i)]++;
+        }
+        
+        for(int i=0; i<s.length(); i++){
+            if(count[s.charAt(i)] >1){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+
+}
