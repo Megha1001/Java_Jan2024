721505fe92d2eefba4ce66023823b3128ddec348
Megha Verma <meghaverma1001@gmail.com>
2024-03-29 22:46:51 +0530

    Update Creation2.java

--- src/linkedList/basicOperations/Creation2.java
+++ src/linkedList/basicOperations/Creation2.java
@@ -1,26 +1,25 @@
-package linkedList;
+package linkedList.basicOperations;
 
 public class Creation2 {
 
+    static class Node{
+        int data;
+        Node next;
+    
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
     
     public static void main(String args[]){
-        Node1 head = new Node1(10);
-        head.next = new Node1(20);
-        head.next.next = new Node1(30);
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
 
         System.out.println(head.data +":"+ head.next);
         System.out.println(head.next.data +":"+ head.next.next);
         System.out.println(head.next.next.data +":"+ head.next.next.next);
     }
     
-}
-
-class Node1{
-    int data;
-    Node1 next;
-
-    Node1(int data){
-        this.data = data;
-        this.next = null;
-    }
 }
