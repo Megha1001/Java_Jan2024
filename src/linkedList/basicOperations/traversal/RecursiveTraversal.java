d02881f69ba567621af4680bd8616c66aec21698
Megha Verma <meghaverma1001@gmail.com>
2024-03-29 23:23:10 +0530

    Create RecursiveTraversal.java

---
+++ src/linkedList/basicOperations/traversal/RecursiveTraversal.java
@@ -0,0 +1,47 @@
+package linkedList.basicOperations.traversal;
+/*
+ * TIME COMPLEXITY : O(N)
+ * AUX SPACE : O(N)
+ */
+public class RecursiveTraversal {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node head = new Node(10);
+        insertAtEnd(head, 20);
+        insertAtEnd(head.next, 30);
+        
+        recursiveTraverse(head);
+
+    }
+
+    public static void insertAtEnd(Node head, int data){
+
+        Node newNode = new Node(data);
+
+        if(head==null){
+            head = newNode;
+        }else{
+            head.next = newNode;
+        }
+
+    }
+
+    public static  void recursiveTraverse(Node head){
+
+        if(head == null){
+            return;
+        }
+
+        System.out.print(head.data+"->");
+        recursiveTraverse(head.next);
+    }
+}
