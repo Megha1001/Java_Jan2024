de193466b5e9c88567d75cb4e251328b103f0162
Megha Verma <meghaverma1001@gmail.com>
2024-03-29 22:57:10 +0530

    Create Traversal.java

---
+++ src/linkedList/basicOperations/Traversal.java
@@ -0,0 +1,31 @@
+package linkedList.basicOperations;
+
+public class Traversal {
+
+
+    static class Node{
+        int data;
+        Node next;//bydefault next sets to null
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    
+    public static void main(String args[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        printList(head);
+    }
+
+    public static void printList(Node head){
+        Node curr = head;
+        while(curr != null){
+            System.out.print(head.data+"->");
+            curr = curr.next;
+        }
+        System.out.print("NULL");
+    }
+    
+}
