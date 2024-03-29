cb9a2624c7b68c1e784124216273bf5c5466e009
Megha Verma <meghaverma1001@gmail.com>
2024-03-29 22:27:42 +0530

    Create Creation.java

---
+++ src/linkedList/Creation.java
@@ -0,0 +1,30 @@
+package linkedList;
+
+
+/*
+ * 10->20->30->null 
+ */
+
+public class Creation {
+    public static void main(String args[]){
+
+        Node head = new Node(10,null);
+        head.next = new Node(20, null);
+        head.next.next = new Node(30, null);
+        
+        System.out.println(head.val +":"+ head.next);
+        System.out.println(head.next.val +":"+ head.next.next);
+        System.out.println(head.next.next.val +":"+ head.next.next.next);
+    }
+}
+
+class Node{
+    int val;
+    Node next;
+
+
+    Node(int val, Node next){
+        this.val = val;
+        this.next = next;
+    }
+}
