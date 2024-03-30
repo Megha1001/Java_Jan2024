262e5402bdec3238fccde8568f6e83785bff9bc6
Megha Verma <meghaverma1001@gmail.com>
2024-03-30 22:35:38 +0530

    Create CircularLL.java

---
+++ src/linkedList/circularLL/CircularLL.java
@@ -0,0 +1,42 @@
+package linkedList.circularLL;
+
+public class CircularLL {
+
+    static class Node{
+        int data;
+        Node next ;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String [] args){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(15);
+        head.next.next.next = head;
+
+        System.out.println("Circular LL ");
+        traverse(head);
+    }
+
+    public static void traverse(Node head){
+        if(head == null){
+            //zero
+            return ;
+        }
+
+        if(head.next == head){
+            System.out.print(head.data);
+        }
+
+
+        Node temp = head;
+        while(temp.next != head){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+}
