ff5bd15b0a0ff6befd3915ac9071ac1a87a4455b
Megha Verma <meghaverma1001@gmail.com>
2024-03-31 20:29:47 +0530

    Create SlowAndFastApp.java

---
+++ src/linkedList/singlyLL/middleOfLL/SlowAndFastApp.java
@@ -0,0 +1,80 @@
+package linkedList.singlyLL.middleOfLL;
+
+public class SlowAndFastApp {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node head = insertAtEnd(null, 10);
+        head = insertAtEnd(head, 20);
+        head = insertAtEnd(head, 30);
+        head = insertAtEnd(head, 40);
+        head = insertAtEnd(head, 50);
+        head = insertAtEnd(head, 60);
+
+        System.out.println("Original LL");
+        traverse(head);
+
+        System.out.println("Middle of ll is ");
+        printMiddle(head);
+
+    }
+
+    public static void printMiddle(Node head){
+        if(head==null){
+            return ;
+        }
+        Node slow = head;
+        Node fast = head;
+
+        /*
+         * NOTE : fast!=null is still require even when all input LL are odd length since we should never access null.next -> it will cause NullPointerException
+         */
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        System.out.println(slow.data);
+
+    }
+
+    public static void traverse(Node head){
+        if(head==null){
+            return;
+        }
+
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+
+        System.out.println("");
+    }
+
+    public static Node insertAtEnd(Node head, int data){
+        Node curr = new Node(data);
+
+        if(head==null){
+            return curr;
+        }
+
+        Node temp = head;
+        while(temp.next !=null){
+            temp = temp.next;
+        }
+
+        temp.next = curr;
+        return head;
+    }
+    
+    
+}
