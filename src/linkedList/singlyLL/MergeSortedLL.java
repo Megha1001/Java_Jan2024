fc3ac00b73f7a6313635d528111c8b6e5472465c
Megha Verma <meghaverma1001@gmail.com>
2024-04-06 18:47:55 +0530

    Create MergeSortedLL.java

---
+++ src/linkedList/singlyLL/MergeSortedLL.java
@@ -0,0 +1,90 @@
+package linkedList.singlyLL;
+
+public class MergeSortedLL {
+
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node head1 = new Node(10);
+        head1.next = new Node(20);
+        head1.next.next = new Node(30);
+
+
+        Node head2 = new Node(5);
+        head2.next = new Node(25);
+        head2.next.next = new Node(28);
+        head2.next.next.next = new Node(35);
+        head2.next.next.next.next = new Node(40);
+        head2.next.next.next.next.next = new Node(43);
+
+
+        Node head = mergeTwoSortedLL(head1, head2);
+        traverse(head);
+    }
+
+    public static Node mergeTwoSortedLL(Node h1, Node h2){
+
+        //base cases
+        if(h1 == null){
+            return h2;
+        }
+        if(h2==null){
+            return h1;
+        }
+
+        Node head = h1; Node tail = h1;
+
+        if(h1.data > h2.data){
+            head = h2;
+            tail = h2;
+            h2 = h2.next;
+        }else{
+            h1 = h1.next;
+        }
+
+
+        while(h1!=null && h2!=null){
+            if(h1.data > h2.data){ // != to maintain stablity
+                tail.next = h2;
+                tail = h2;
+                h2 = h2.next;
+            }else{
+                tail.next = h1;
+                tail = h1;
+                h1 = h1.next;
+            }
+        }
+
+
+        if(h1!=null){
+            tail.next = h1;
+        }else{
+            tail.next = h2;
+        }
+
+        return head;
+    }
+
+
+    public static void traverse(Node head){
+        if(head==null){
+            return;
+        }
+
+        Node curr = head;
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr = curr.next;
+        }
+
+        System.out.println();
+    }
+    
+}
