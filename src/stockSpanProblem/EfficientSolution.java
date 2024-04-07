bdf71175a6a9b5ebc72020f276d1c86dc9cac766
Megha Verma <meghaverma1001@gmail.com>
2024-04-07 22:00:51 +0530

    Create EfficientSolution.java

---
+++ src/stockSpanProblem/EfficientSolution.java
@@ -0,0 +1,12 @@
+package stockSpanProblem;
+
+/*
+Approach :
+1. if there is a greater element on left side
+    -> Span = Index of current element - index of closest greater element on left side
+
+2. Otherwise
+    ->  Span = index of current element +1 (since its the largest element till now.)
+ */
+public class EfficientSolution {
+}
