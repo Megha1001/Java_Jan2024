a17c831cde988f75d6a57247e36d7d7fe3aa5148
Megha Verma <meghaverma1001@gmail.com>
2024-04-09 00:54:06 +0530

    Create NaiveSolution.java

---
+++ src/stockSpanProblem/variations/nextGreaterOnRight/NaiveSolution.java
@@ -0,0 +1,37 @@
+package stockSpanProblem.variations.nextGreaterOnRight;
+
+public class NaiveSolution {
+
+    public static void main(String args[]){
+        int arr[] = {5,15,10,8,6,12,9,18};
+
+        System.out.println("The nearest greatest element on righ side of each elements are : ");
+        getAndPrintGreatestNearestElement(arr);
+    }
+
+    public static void getAndPrintGreatestNearestElement(int arr[]){
+        int n = arr.length;
+
+        if(n == 0){
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+            int j;
+            for (j=i+1; j<n; j++){
+                if(arr[i]<arr[j]){
+                    System.out.print(arr[j]+" ");
+                    break;
+                }
+            }
+
+            // if the current is greatest
+            if(j==n){
+                System.out.print(-1+" ");
+            }
+        }
+
+
+    }
+    
+}
