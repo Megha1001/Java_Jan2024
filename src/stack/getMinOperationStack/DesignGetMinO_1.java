0db4279a21f6c748a3e73b78fc2cae3077a51d34
Megha Verma <meghaverma1001@gmail.com>
2024-04-09 17:06:50 +0530

    Create DesignGetMinO_1.java

---
+++ src/stack/getMinOperationStack/DesignGetMinO_1.java
@@ -0,0 +1,22 @@
+package stack.getMinOperationStack;
+
+import java.util.Stack;
+
+public class DesignGetMinO_1 {
+
+    static class MyStack{
+        Stack<Integer> mainStack = new Stack<>();
+        Stack<Integer> auxStack = new Stack<>();
+
+
+        //push operation -> element x
+        /*
+         * 1. For first element push to both ms and as
+         * 2. Always push to ms
+         *  - Push to as only when as.peek() >= x
+         * 
+         */
+
+    }
+    
+}
