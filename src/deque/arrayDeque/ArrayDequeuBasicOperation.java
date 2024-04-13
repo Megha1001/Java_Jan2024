a1d8f7cf667321eb9ceed690a67ee8819983e8b5
Megha Verma <meghaverma1001@gmail.com>
2024-04-13 18:03:19 +0530

    Create ArrayDequeuBasicOperation.java

---
+++ src/deque/arrayDeque/ArrayDequeuBasicOperation.java
@@ -0,0 +1,16 @@
+package deque.arrayDeque;
+
+import java.util.ArrayDeque;
+
+public class ArrayDequeuBasicOperation {
+    
+    public static void main(String args[]){
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        ad.add(10);
+        ad.add(20);
+        ad.add(30);
+
+        System.out.println(ad);
+
+    }
+}
