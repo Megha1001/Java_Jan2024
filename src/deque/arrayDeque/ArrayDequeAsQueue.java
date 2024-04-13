6d9a0c88401d915de8161ff3710450db30d0014d
Megha Verma <meghaverma1001@gmail.com>
2024-04-13 18:12:27 +0530

    Create ArrayDequeAsQueue.java

---
+++ src/deque/arrayDeque/ArrayDequeAsQueue.java
@@ -0,0 +1,18 @@
+package deque.arrayDeque;
+
+import java.util.ArrayDeque;
+
+
+public class ArrayDequeAsQueue {
+    public static void main(String args[]){
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+
+        ad.offer(10);
+        ad.offer(20);
+        System.out.println(ad.peek());
+        System.out.println(ad.poll());
+        System.out.println(ad.peek());
+        ad.offer(40);
+        System.out.println(ad.peek());
+    }
+}
