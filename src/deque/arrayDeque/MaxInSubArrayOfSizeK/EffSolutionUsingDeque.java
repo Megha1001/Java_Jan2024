15e0b43fac69d73a5a8563b4f161e32f58b9cf60
Megha Verma <meghaverma1001@gmail.com>
2024-04-14 13:21:04 +0530

    Create EffSolutionUsingDeque.java

---
+++ src/deque/arrayDeque/MaxInSubArrayOfSizeK/EffSolutionUsingDeque.java
@@ -0,0 +1,69 @@
+package deque.arrayDeque.MaxInSubArrayOfSizeK;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+* Approach : 
+* 
+* If we see current element is greater than element present at last --> remove that since it will not contribute being maximum element in present and in subsequent subarray 
+*  otherwise add index to deque
+*  -> WHY INDEX : since we need to make sure it should be deleted for next subarray consideration since element whose index<=i-k would be invalid
+*   
+*
+*   peek --> peekFirst
+*   peekLast
+*   pollLast
+*   offerFirst
+*/
+
+public class EffSolutionUsingDeque {
+    public static void main(String args[]){
+        int arr[] = {20,40,30,10,60};
+        int k =3;
+
+        System.out.println("The maximum elements in subarray of size K are : ");
+        printMaxForSubarrayK(arr, k);
+    }
+
+    
+    public static void printMaxForSubarrayK(int arr[], int k){
+
+        Deque<Integer> d = new LinkedList<>();
+        int n = arr.length;
+
+
+        //process for first 
+        
+        for(int i=0; i<k; i++){
+
+            while(!d.isEmpty() && arr[i] >= arr[d.peekLast()]){
+                    d.pollLast();
+            }
+
+            d.offerLast(i);
+        }
+
+
+        for(int i=k; i<n; i++){
+            //print for previous subarray
+            System.out.print(arr[d.peek()]+ " ");//this is peekFirst
+
+            //remove invalid elements
+            while(!d.isEmpty() && d.peek() <= i-k){//this is peekFirst
+                d.pollFirst();
+            }
+
+            //check for elements(i-k-1) in present subequence that are alrady there in dequeu --> if they are <=curr_element remove them
+            while(!d.isEmpty() && arr[i] >= arr[d.peekLast()]){
+                d.pollLast();
+            }
+
+            d.offerLast(i);
+        }
+
+        //for last subsequence --> print max element
+        System.out.print(arr[d.peek()]);
+
+    }
+}
