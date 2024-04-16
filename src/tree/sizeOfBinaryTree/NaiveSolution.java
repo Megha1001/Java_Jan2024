1118efd49f4d9f1bdcea17dab3e1a8e80f4b7f40
Megha Verma <meghaverma1001@gmail.com>
2024-04-16 21:33:42 +0530

    Create NaiveSolution.java

---
+++ src/tree/sizeOfBinaryTree/NaiveSolution.java
@@ -0,0 +1,44 @@
+package tree.sizeOfBinaryTree;
+
+/*
+ * TIME COMPLEXITY : O(N), traverserving every node exactly once.
+ * AUX SPACE : O(H)
+ */
+
+public class NaiveSolution {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+
+        root.right.right = new Node(60);
+
+        System.out.println("The number of nodes tree has : "+calNumOfNodes(root));
+
+    }
+
+    public static int calNumOfNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        else{
+            return 1 + calNumOfNodes(root.left) + calNumOfNodes(root.right);
+        }
+    }
+    
+}
