cec44abb37c945d738b63646ef8cffe36f7c1da6
Megha Verma <meghaverma1001@gmail.com>
2024-04-17 22:53:49 +0530

    Create NaiveSolution.java

---
+++ src/tree/widthOfTree/NaiveSolution.java
@@ -0,0 +1,68 @@
+package tree.widthOfTree;
+
+import java.util.Queue;
+import java.util.LinkedList;
+
+/*
+ * use solution of printing left view iterative solution
+ */
+
+public class NaiveSolution {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node root = new Node(10);
+        root.left = new Node(20);
+        root.right = new Node(30);
+
+        root.left.left = new Node(40);
+        root.left.right = new Node(50);
+
+        root.right.right = new Node(60);
+       root.right.right.right = new Node(70);
+
+        System.out.println("Width of given tree is ? "+findWidth(root));
+    }
+
+    public static int findWidth(Node root){
+        int res = 0;
+        
+        if(root==null){
+            return res;
+        }
+
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int size = q.size();
+
+            res = Math.max(res, size);
+
+            for(int i=0; i<size;i++){
+                Node temp = q.poll();
+
+                if(temp.left!=null){
+                    q.add(temp.left);
+                }
+                if(temp.right!=null){
+                    q.add(temp.right);
+                }
+            }
+            
+        }
+
+    }
+
+    return res;
+    
+}
