aebb48aa0bd2b18f2ac5663f5571b09f78cb7524
Megha Verma <meghaverma1001@gmail.com>
2024-04-19 23:36:43 +0530

    Update TreeConstWithInorderAndPreOrder.java

---
+++ src/tree/treeContructionFromOrders/TreeConstWithInorderAndPreOrder.java
@@ -0,0 +1,50 @@
+package tree.treeContructionFromOrders;
+
+public class TreeConstWithInorderAndPreOrder {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+
+    public static void main(String args[]){
+        int in[] ={20,10,40,30,50};
+        int pre[] = {10,20,30,40,50};
+
+        System.out.println("The binary tree is : ");
+        constructTree(in, pre, in.length, pre.length-1);
+    }
+    
+    public static int preIndex=0; // to iterate through pre-order
+
+    public static Node constructTree(int in[], int pre[], int is, int ie){
+
+        if(is>ie){
+            return null;
+        }
+
+        //consider element of pre-order as root
+        Node root = new Node(pre[preIndex++]);
+
+        //search root in inorder
+        int inIndex=0;
+        for(int i=is; i<=ie; i++){
+            if(root.data == in[i]){
+                inIndex = i;
+                break;
+            }
+        }
+
+        root.left = constructTree(in, pre, is, inIndex-1);
+        root.right = constructTree(in, pre, inIndex+1, ie);
+
+        return root;
+    }
+    
+}
