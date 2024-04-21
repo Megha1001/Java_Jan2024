e1f511f86e035f0093e88f3db10cc5b3befda155
Megha Verma <meghaverma1001@gmail.com>
2024-04-21 17:58:08 +0530

    Create InorderTraversal.java

---
+++ src/tree/traversal/levelOrderTraversal/iterativeSolution/InorderTraversal.java
@@ -0,0 +1,57 @@
+package tree.traversal.levelOrderTraversal.iterativeSolution;
+
+import java.util.Stack;
+
+
+public class InorderTraversal {
+    
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+        }
+
+    }
+
+    public static void main(String args[]){
+        int arr[] = {10,20,40,-1,-1,50,-1,-1,30,-1,-1};
+        Node root = buildTree(arr);
+
+        inorderTraversal(root);
+    }
+    
+    public static void inorderTraversal(Node root){
+        Stack<Node> s = new Stack<>();
+
+        Node curr = root;
+
+        while(curr!=null || !s.isEmpty()){
+            while(curr!=null){
+                s.push(curr);
+                curr = curr.left;
+            }
+
+            curr = s.pop();
+            System.out.print(curr.data+" ");
+            curr = curr.right;
+        }
+    }
+
+    public static int idx=-1;
+    public static Node buildTree(int []arr){
+        ++idx;
+        if(arr[idx]==-1){
+            return null;
+        }
+
+        Node newNode = new Node(arr[idx]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+
+        return newNode;
+    }
+
+}
