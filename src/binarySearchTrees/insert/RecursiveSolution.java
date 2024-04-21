ba1c8fb93f245ba82d6404f1d88ad1d617c7fbd2
Megha Verma <meghaverma1001@gmail.com>
2024-04-22 00:12:59 +0530

    Create RecursiveSolution.java

---
+++ src/binarySearchTrees/insert/RecursiveSolution.java
@@ -0,0 +1,52 @@
+package binarySearchTrees.insert;
+
+public class RecursiveSolution {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        Node root = new Node(10);
+        root.left = new Node(5);
+        root.right = new Node(15);
+        
+        root.right.left = new Node(12);
+        root.right.right = new Node(18);
+
+        System.out.println("After inseration the tree is : ");
+        Node newRoot = insert(root, 20);
+    }
+
+    public static Node insert(Node root, int x){
+        Node newNode = new Node(x);
+        if(root==null){
+            return newNode;
+        }
+
+        Node curr = root;
+        while(curr.left!=null || curr.right!=null){
+            if(curr.data < x){
+                curr = curr.right;
+            }else{
+                curr = curr.left;
+            }
+        }
+
+        if(curr.data < x){
+            curr.right = newNode;
+        }else{
+            curr.left = newNode;
+        }
+        
+
+        return root;
+    }
+    
+}
