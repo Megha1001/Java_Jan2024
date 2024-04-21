987e2825af4a78f33e54dcacd2f2649666d1b55b
Megha Verma <meghaverma1001@gmail.com>
2024-04-21 18:51:33 +0530

    Create PreOrderTraversal.java

---
+++ src/tree/traversal/levelOrderTraversal/iterativeSolution/PreOrderTraversal.java
@@ -0,0 +1,58 @@
+package tree.traversal.levelOrderTraversal.iterativeSolution;
+
+import java.util.Stack;
+
+
+public class PreOrderTraversal {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[] ={10,20,40,80,-1,-1,90,-1,-1,50,-1,-1,30,60,-1,-1,70,-1,-1};
+
+        Node root = buildTree(arr);
+
+        System.out.println("The preorder traversal iteratively is ");
+        preOrderTraversal(root);
+
+    }
+
+    public static void preOrderTraversal(Node root){
+        Stack<Node> s = new Stack<>();
+
+        Node curr = root;
+        while(curr!=null || !s.isEmpty()){
+            while(curr!=null){
+                System.out.print(curr.data+" ");
+                s.push(curr);
+                curr = curr.left;
+            }
+
+            curr = s.pop();
+            curr = curr.right;
+        } 
+
+    }
+    
+    public static int idx = -1;
+    public static Node buildTree(int []arr){
+        ++idx;
+        if(arr[idx]==-1){
+            return null;
+        }
+
+        Node newNode = new Node(arr[idx]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+
+        return newNode;
+    }
+}
