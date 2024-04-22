c91eeedb12487f3343991db087d6c10e18ff22f1
Megha Verma <35575924+Megha1001@users.noreply.github.com>
2024-04-22 17:25:22 +0530

    Create RecursiveSolution2.java

---
+++ src/tree/binarySearchTrees/insert/RecursiveSolution2.java
@@ -0,0 +1,63 @@
+class RecursiveSolution2 {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node(int data){
+            this.data = data;
+        }
+    }
+    
+    public static void main(String args[]){
+        int arr[] = {10,5,-1,-1,15,12,-1,-1,18,-1,-1};
+        
+        Node root = buildTree(arr);
+        root = insertNode(root, 20);
+        traversePreOrder(root);
+    }
+    
+    public static void traversePreOrder(Node root){
+        if(root==null){
+            return;
+        }
+        
+        System.out.print(root.data+" ");
+        traversePreOrder(root.left);
+        traversePreOrder(root.right);
+    }
+    
+    public static int idx = -1;
+    
+    public static Node buildTree(int arr[]){
+        ++idx;
+        if(arr[idx]==-1){
+            return null;
+        }
+        
+        Node newNode = new Node(arr[idx]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+        
+        return newNode;
+    }
+    
+    public static Node insertNode(Node root, int x){
+        Node newNode = new Node(x);
+        
+        if(root == null){
+            return newNode;
+        }
+        
+        if(root.data < x){
+            root.right = insertNode(root.right, x);
+        }
+        
+        if(root.data > x){
+            root.left = insertNode(root.left, x);
+        }
+        
+        
+        return root; //incase of equal as well it will not insert that element simply return.
+    }
+}
