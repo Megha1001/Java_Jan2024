a3bd1b66718d6b439e19fe1a0c1a21dc7fbaec07
Megha Verma <meghaverma1001@gmail.com>
2024-04-22 21:06:34 +0530

    Create RecursiveSolution.java

---
+++ src/tree/binarySearchTrees/delete/RecursiveSolution.java
@@ -0,0 +1,104 @@
+package tree.binarySearchTrees.delete;
+
+public class RecursiveSolution {
+
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    
+
+    public static void main(String args[]){
+        int arr[]={50,30,-1,40,-1,-1,70,60,-1,-1,80,-1,-1};
+
+        Node root = buildTree(arr);
+
+        root = deleteNode(root, 50);
+        inOrderTraversal(root);
+    }
+
+    public static void inOrderTraversal(Node root){
+        if(root==null){
+            return ;
+        }
+
+        System.out.print(root.data+" ");
+        inOrderTraversal(root.left);
+        inOrderTraversal(root.right);
+
+    }
+
+
+    public static Node deleteNode(Node root, int x){
+        if(root==null){
+            return null;
+        }
+
+        if(root.data < x){
+            //go to right
+            root.right = deleteNode(root.right, x);
+        }
+
+        if(root.data > x){
+            //go to left
+            root.left = deleteNode(root.left, x); //root.left= --> this is needed to like IMP!
+        }
+        
+        //when we find the key
+        else{
+
+            //have one child or no child
+            if(root.left == null){
+                return root.right;
+            }
+
+            if(root.right==null){
+                return root.left;
+            }
+
+            //when both the child are present --> consider successor
+            Node succ = getSuccessor(root, x);
+            root.data = succ.data;
+            /*
+            since root's data is replaced by successor's data --> and at this point we have 
+            two occurrence of successor.One at root.data place and other at its original place
+            (right of root), we have to delete its original occurrence
+            */
+
+            root.right = deleteNode(root.right, succ.data);
+
+        }
+        return root;
+
+    }
+
+    /*
+     * To find successor --> go right then go left, left left until not found null
+     */
+    public static Node getSuccessor(Node root, int x){
+        Node curr = root.right; // root should be there, otherwise got caught at earlier stages
+        while(curr!=null && curr.left!=null){
+            curr = curr.left;
+        }
+        return curr;
+    }
+
+    public static int idx = -1;
+    public static Node buildTree(int arr[]){
+        ++idx;
+        if(arr[idx]==-1){
+            return null;
+        }
+
+        Node newNode = new Node(arr[idx]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+
+        return newNode;
+    }
+}
