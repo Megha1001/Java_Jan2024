3f178f7286acaad1a4efbd21c08976be9637730a
Megha Verma <meghaverma1001@gmail.com>
2024-04-26 21:54:28 +0530

    Update EfficientApproach.java

--- src/tree/binarySearchTrees/seflBalancingTrees/designDataStructure/KthSmallest/EfficientApproach.java
+++ src/tree/binarySearchTrees/seflBalancingTrees/designDataStructure/KthSmallest/EfficientApproach.java
@@ -8,4 +8,34 @@ Augmented tree : Maintain  lCount of nodes in the left subtree
         -> if smaller, recur for right subtree with K as (k-lCount-1)
  */
 public class EfficientApproach {
+
+    static class Node
+    {
+        int key;
+        Node left;
+        Node right;
+
+        int lCount;
+        Node(int k){
+            key=k;
+            lCount=0;
+        }
+    }
+    public static void main(String[] args) {
+        Node root=new Node(15);
+        root.left=new Node(5);
+        root.left.left=new Node(3);
+        root.right=new Node(20);
+        root.right.left=new Node(18);
+        root.right.left.left=new Node(16);
+        root.right.right=new Node(80);
+        int k=3;
+
+        System.out.print("Kth Smallest: ");
+        printKth(root,k);
+    }
+
+    public static void printKth(Node root, int k){
+
+    }
 }
