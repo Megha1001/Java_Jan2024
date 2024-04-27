9e5dcb58140db187db0ae0f5908b10f3f409aa4c
Megha Verma <meghaverma1001@gmail.com>
2024-04-27 20:03:02 +0530

    Create VerticalSumUsingTreeMap.java

---
+++ src/tree/binarySearchTrees/seflBalancingTrees/verticalSum/VerticalSumUsingTreeMap.java
@@ -0,0 +1,13 @@
+package tree.binarySearchTrees.seflBalancingTrees.verticalSum;
+
+/*
+Approach
+1. use any traversal while traversing if going on left do horizontal_heigh-1 and if going on right horizontal_heigh+1.
+2. Maintain a treemap<Integer, Integer>, 1st represents hd and second sum.
+3. Print the Map
+
+Why using hasmap --> since it prints in sorted order
+ */
+
+public class VerticalSumUsingTreeMap {
+}
