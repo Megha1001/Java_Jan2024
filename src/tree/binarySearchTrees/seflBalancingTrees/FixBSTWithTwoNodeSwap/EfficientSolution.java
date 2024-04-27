6b019ad8b9308754aa3ff24c6e2e89239931f3fb
Megha Verma <meghaverma1001@gmail.com>
2024-04-27 16:24:29 +0530

    Update EfficientSolution.java

--- src/tree/binarySearchTrees/seflBalancingTrees/FixBSTWithTwoNodeSwap/EfficientSolution.java
+++ src/tree/binarySearchTrees/seflBalancingTrees/FixBSTWithTwoNodeSwap/EfficientSolution.java
@@ -2,7 +2,7 @@ package tree.binarySearchTrees.seflBalancingTrees.FixBSTWithTwoNodeSwap;
 
 /*
 Approach :
-Idea : Inorder traversal of BST is sorted
+Idea : Inorder traversal of BST is sorted --> USE CHECKBST MostEfficient Solution Idea
 
 1. Maintain three references -> prev, first, second
 2. while traversing BST in inorder fashion
@@ -28,6 +28,50 @@ public class EfficientSolution {
         int arr[] = {20,60,4,-1,-1,10,-1,-1,80,8,-1,-1,100,-1,-1};
 
         Node root = buildTree(arr);
+        inOrder(root);
+
+        fixBST(root);
+
+        int temp = first.data;
+        first.data = second.data;
+        second.data = temp;
+
+        System.out.println();
+        inOrder(root);
+    }
+
+    static Node prev=null, first = null, second = null;
+
+    public static void fixBST(Node root){ //no need to return anything since we just want to populate first and second
+        if(root == null){
+            return;
+        }
+
+        fixBST(root.left);
+
+        if(prev!=null && prev.data > root.data){
+            if(first==null){
+                first=prev;
+            }
+
+            second = root; //always populate since when consecutive nodes needs to swap
+        }
+
+        prev = root;
+
+        fixBST(root.right);
+
+    }
+
+    public static void inOrder(Node root){
+        if(root==null){
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+
     }
 
     public static int idx = -1;
