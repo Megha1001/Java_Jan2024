8b5297d660f7fc2d31fc001263078c83ce792b32
Megha Verma <meghaverma1001@gmail.com>
2024-04-27 16:16:01 +0530

    Update EfficientSolution.java

--- src/tree/binarySearchTrees/seflBalancingTrees/FixBSTWithTwoNodeSwap/EfficientSolution.java
+++ src/tree/binarySearchTrees/seflBalancingTrees/FixBSTWithTwoNodeSwap/EfficientSolution.java
@@ -11,8 +11,38 @@ Idea : Inorder traversal of BST is sorted
  */
 public class EfficientSolution {
 
-    public static void main(String[] args) {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+
+        Node(int data){
+            this.data = data;
+        }
 
     }
-    
+
+    public static void main(String[] args) {
+        //Pre-order traversal -> NLR
+        int arr[] = {20,60,4,-1,-1,10,-1,-1,80,8,-1,-1,100,-1,-1};
+
+        Node root = buildTree(arr);
+    }
+
+    public static int idx = -1;
+    public static Node buildTree(int arr[]){
+        ++idx;
+        if(arr[idx]==-1){
+            return null;
+        }
+
+        Node newNode = new Node(arr[idx]);
+        newNode.left = buildTree(arr);
+        newNode.right = buildTree(arr);
+
+        return  newNode;
+
+
+    }
 }
