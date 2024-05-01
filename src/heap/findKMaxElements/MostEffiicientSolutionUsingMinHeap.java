c65b48ab590bc8ed826663da8ad162c4d263265d
Megha Verma <meghaverma1001@gmail.com>
2024-05-01 15:09:12 +0530

    Update MostEffiicientSolutionUsingMinHeap.java

--- src/heap/findKMaxElements/MostEffiicientSolutionUsingMinHeap.java
+++ src/heap/findKMaxElements/MostEffiicientSolutionUsingMinHeap.java
@@ -11,5 +11,41 @@ Approach :
 TIME COMPLEXITY : O(k) + O((n-k)log(k))
  */
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class MostEffiicientSolutionUsingMinHeap {
+
+    public static void main(String[] args) {
+        int [] arr = {8,6,4,10,9};
+        int k=3;
+
+        System.out.println("The "+k+" largest elements in given array are : ");
+        findKLargestItems(arr, k);
+    }
+
+    public static void findKLargestItems(int arr[], int k){
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); //min heap
+
+        //O(K)
+        for(int i=0; i<k; i++){
+            pq.offer(arr[i]);
+        }
+
+        int i=k;
+        while(i<arr.length){ //O(n-k)
+            if(arr[i]>pq.peek()){
+                pq.poll();
+                pq.offer(arr[i]); // extract min --> O(logK)
+                ++i;
+            }
+        }
+
+        //iterate
+        Iterator<Integer> iterator = pq.iterator();
+
+        while(iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+    }
 }
