5c73d44bc2bd6b31939b186420a01439e381459e
Megha Verma <meghaverma1001@gmail.com>
2024-05-06 21:57:02 +0530

    Update DFSOfDisconnectedGraph.java

--- src/graphs/adjacencyList/DFS/DFSOfDisconnectedGraph.java
+++ src/graphs/adjacencyList/DFS/DFSOfDisconnectedGraph.java
@@ -1,4 +1,57 @@
 package graphs.adjacencyList.DFS;
 
+import java.util.ArrayList;
+
+/*
+ * TIME COMPLEXITY : O(V+E)
+ *
+ * Reason for O(E) : we are visiting every edge (Total number of edges : 2E(undirected graph), E(directed graph))
+ *
+ * Why V : There might be the case when there are no edges all vertices are disconnected. In that case too we still run the outermost loop
+ */
+
+
+
 public class DFSOfDisconnectedGraph {
+    public static void main(String[] args) {
+        int V = 6;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>(V);
+
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        addAnEdge(adj, 0, 1);
+        addAnEdge(adj, 0, 2);
+
+        addAnEdge(adj, 3, 4);
+
+        System.out.println("DFS traversal of given adjacency list is : ");
+
+        boolean [] visited = new boolean[V];
+
+        for(int i=0; i<V; i++){
+            if(!visited[i]){
+                DFSOfDisconnectedGraph(adj, i, visited);
+            }
+        }
+    }
+
+    public static void DFSOfDisconnectedGraph(ArrayList<ArrayList<Integer>>adj, int s, boolean [] visited){
+
+        visited[s] = true;
+        System.out.print(s+" ");
+
+        for(int u : adj.get(s)){
+            if(!visited[u]){
+                DFSOfDisconnectedGraph(adj, u, visited);
+            }
+        }
+
+    }
+
+    public static void addAnEdge(ArrayList<ArrayList<Integer>>adj, int u, int v){
+        adj.get(u).add(v);
+        adj.get(v).add(u);
+    }
 }
