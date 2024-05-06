338c5fa991314b2d84b1f21b1e8889cfddf32fba
Megha Verma <meghaverma1001@gmail.com>
2024-05-06 23:54:35 +0530

    Update ShortestPathInUnWeightedGraph.java

--- src/graphs/adjacencyList/BFS/ShortestPathInUnWeightedGraph.java
+++ src/graphs/adjacencyList/BFS/ShortestPathInUnWeightedGraph.java
@@ -1,4 +1,17 @@
 package graphs.adjacencyList.BFS;
 
+import java.util.ArrayList;
+
 public class ShortestPathInUnWeightedGraph {
+
+    public static void main(String[] args) {
+        int V=4;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+        for(int i=0; i<V; i++){
+            adj.add(new ArrayList<>());
+        }
+
+    }
+
 }
